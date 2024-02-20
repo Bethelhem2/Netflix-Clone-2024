e00dfdd63b09691a237b1a53ccfb6a43d3d14ffb
Bethelhem2 <bettywoldeamanuel@gmail.com>
2024-02-20 22:45:03 +0300

    header and footer

---
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import './Footer.css'
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+
+function Footer() {
+  return (
+    <div className="externalWrapper container">
+      <div className="logoWrapper d-flex justify-content-between">
+        <div>
+          <FacebookRoundedIcon />
+        </div>
+        <div>
+          <InstagramIcon />
+        </div>
+
+        <div>
+          <YouTubeIcon />
+        </div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div>
+          <ul>
+            <li>Audio Description</li>
+            <li>Investor Relations</li>
+            <li>Legal Notice</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Help Center</li>
+            <li>Jobs</li>
+            <li>Cookie Preferences</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Gift Cards</li>
+            <li>Terms of Use</li>
+            <li>Corporate Information</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li>Media Center</li>
+            <li>Privacy</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+      <div className="border d-inline p-x-5">Server Code</div>
+      <div>
+        <CopyrightIcon />
+        1997-2024 Netflix, Inc.
+      </div>
+    </div>
+  );
+}
+
+export default Footer
---
+++ netflix-clone/src/Components/Header/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../../bootstrap.css";
+import './Header.css'
+import netflixLogo from '../../Assets/images/netflixLogo.png'
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+function Header() {
+  return (
+    <div className="external_wrapper sticky-top container-fluid">
+      <div className="internal_wrapper d-flex justify-content-between">
+        <div className="left_side_wrapper ">
+          <ul className="d-flex justify-content-between ">
+            <li>
+              <img src={netflixLogo} alt="NetflixLogo" width="100" />
+            </li>
+            <li className="text-info text-decoration-none mr-3">Home</li>
+            <li className="mr-3">TvShows</li>
+            <li className="mr-3"> Movies</li>
+            <li className="mr-3">Latest</li>
+            <li className="mr-3">MyList</li>
+            <li className="mr-3">Browse By Languages</li>
+          </ul>
+        </div>
+        <div className="right_side_wrapper ">
+          <ul className="d-flex justify-content-between">
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
