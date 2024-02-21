36a951c773040c9a1e95f5204caa933cafa526dc
Bethelhem2 <bettywoldeamanuel@gmail.com>
2024-02-21 04:24:16 +0300

    axios and request

---
+++ netflix-clone/src/Utils/Request.js
@@ -0,0 +1,14 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+import React from 'react'
+const requests = {
+
+
+
+ requestMovies:'https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&page=1'
+   
+
+
+
+
+}
+export default Request
